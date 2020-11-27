26340fb80f3f85bf030847e9ba87b6ff09a03b58
Vicky Lee <vicky.winkay.lee@gmail.com>
2020-11-27 11:20:57 +0000

    Add Storybook

---
+++ src/components/QueryEditorRow/QueryEditorRow.stories.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { QueryEditorRow } from './QueryEditorRow';
+
+export default {
+  title: 'Layout/QueryEditorRow',
+  component: QueryEditorRow,
+};
+
+export const basic = () => <QueryEditorRow />
