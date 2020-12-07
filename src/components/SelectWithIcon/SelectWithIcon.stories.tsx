25f7ea6e1e76f4e9ed147c899624f758f815de12
Vicky Lee <vicky.winkay.lee@gmail.com>
2020-12-07 16:22:07 +0000

    update stories to add in examples of overriding icons

--- src/components/SelectWithIcon/SelectWithIcon.stories.tsx
+++ src/components/SelectWithIcon/SelectWithIcon.stories.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { SelectWithIcon } from './SelectWithIcon';
 import { generateOptions } from '../../__fixtures__/Select';
-import { Input } from '@grafana/ui';
+import { IconName, Input } from '@grafana/ui';
 import { SelectableValue } from '@grafana/data';
+import { getIcon } from '../../__fixtures__/Icon';
 
 export default {
   title: 'Forms/SelectWithIcon',
@@ -77,5 +78,43 @@ export const withChildren = () => {
     >
       {value && <Input css={null} width={40} />}
     </SelectWithIcon>
+  );
+};
+
+export const customAddIcon = () => {
+  const [value, setValue] = React.useState<string>();
+  const [icon, setIcon] = React.useState<IconName>();
+
+  React.useEffect(() => setIcon(getIcon()), []);
+
+  return (
+    <SelectWithIcon
+      width={40}
+      addIcon={icon}
+      displayIcon={!value}
+      options={generateOptions()}
+      value={value}
+      onChange={(ev) => setValue(ev.value)}
+    />
+  );
+};
+
+export const customRemoveIcon = () => {
+  const [value, setValue] = React.useState<string>();
+  const [icon, setIcon] = React.useState<IconName>();
+
+  React.useEffect(() => setIcon(getIcon()), []);
+
+  return (
+    <SelectWithIcon
+      width={40}
+      displayIcon={!value}
+      options={generateOptions()}
+      value={value}
+      onChange={(ev) => setValue(ev.value)}
+      renderRemove={Boolean(value)}
+      onRemove={() => setValue('')}
+      removeIcon={icon}
+    />
   );
 };
