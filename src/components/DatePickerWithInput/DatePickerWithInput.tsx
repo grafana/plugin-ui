380fa1170fdab0d6dc255532b8acabd4ebd3fdd8
Vicky Lee <vicky.winkay.lee@gmail.com>
2020-12-08 11:48:59 +0000

    add DatePickerWithInput component

---
+++ src/components/DatePickerWithInput/DatePickerWithInput.tsx
@@ -0,0 +1,44 @@
+import { ClickOutsideWrapper, Input } from '@grafana/ui';
+import React from 'react';
+import { DatePicker } from '../DatePicker/DatePicker';
+import { Props as InputProps } from '@grafana/ui/components/Input/Input';
+import './styles.css';
+
+const formatDate = (date: Date) => date.toISOString().split('T')[0];
+
+export interface DatePickerWithInputProps
+  extends Omit<InputProps, 'ref' | 'value' | 'onChange'> {
+  value?: Date;
+  onChange: (value: Date) => void;
+}
+
+export const DatePickerWithInput = (props: DatePickerWithInputProps) => {
+  const { value, onChange, ...rest } = props;
+
+  const [open, setOpen] = React.useState(false);
+
+  return (
+    <>
+      <Input
+        type='date'
+        placeholder='Date'
+        css={null}
+        value={formatDate(value || new Date())}
+        onClick={() => setOpen(true)}
+        onChange={() => {}}
+        {...rest}
+      />
+      <ClickOutsideWrapper
+        useCapture={true}
+        includeButtonPress={false}
+        onClick={() => setOpen(false)}
+      >
+        <DatePicker
+          isOpen={open}
+          value={value}
+          onChange={(ev) => onChange(ev)}
+        />
+      </ClickOutsideWrapper>
+    </>
+  );
+};
