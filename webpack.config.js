d3af433c1b800e9702ec611bebc89acec4747e4d
Vicky Lee <vicky.winkay.lee@gmail.com>
2020-12-09 14:36:31 +0000

    add webpack

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  target: 'node',
+  externals: [nodeExternals()],
+};
