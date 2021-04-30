f8adf60a30786b9730986cb8af9c6de14a46bc72
Vicky Lee <vicky.winkay.lee@gmail.com>
2021-04-30 14:03:07 +0100

    add typing from core Grafana

--- src/utils/testDatasource.ts
+++ src/utils/testDatasource.ts
@@ -1,6 +1,19 @@
 import { HealthCheckResult } from '@grafana/runtime';
 
-export type BaseTestDatasource = () => Promise<Partial<HealthCheckResult> | Error>
+// These are from core Grafana - they are not exported
+export type HealthCheckResultDetails = Record<string, any> | undefined
+export class HealthCheckError extends Error {
+  details: HealthCheckResultDetails;
+
+  constructor(message: string, details: HealthCheckResultDetails) {
+    super(message);
+    this.details = details;
+    this.name = 'HealthCheckError';
+  }
+}
+
+export type TestDatasourceReturn = Partial<HealthCheckResult> | Error
+export type BaseTestDatasource = () => Promise<TestDatasourceReturn>
 
 /**
  * Override function for testDatasource in Grafana for the health check
@@ -12,7 +25,7 @@ export type BaseTestDatasource = () => Promise<Partial<HealthCheckResult> | Erro
  * @param baseTestDatasource The original testDatasource function
  * @returns Either the health check result if the health check was successful or an error that is handled later by Grafana
  */
-export const testDatasource = async (baseTestDatasource: BaseTestDatasource) => {
+export const testDatasource = async (baseTestDatasource: BaseTestDatasource): Promise<TestDatasourceReturn> => {
   // the backwards compatibility only affects the health check error messages
   // so only transform the error we give to Grafana
   try {
