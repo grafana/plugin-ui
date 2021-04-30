a23eda6b10b085eac072b1f1ef0ff7b580642eee
Vicky Lee <vicky.winkay.lee@gmail.com>
2021-04-30 14:02:41 +0100

    move things into compatFeatures file

--- src/utils/compatibility.ts
+++ src/utils/compatibility.ts
@@ -1,8 +1,7 @@
 import { config } from "@grafana/runtime";
 import { gte } from "semver";
-import { testDatasource, BaseTestDatasource } from "./testDatasource";
 
-enum CompatibilityFeature {
+export enum CompatibilityFeature {
   HEALTH_DIAGNOSTICS_ERRORS
 }
 
@@ -14,7 +13,7 @@ enum CompatibilityFeature {
  * @param feature The feature that requires backwards compatibility
  * @returns True if the Grafana version running can support the feature, otherwise false
  */
- const hasCapability = (feature: CompatibilityFeature): boolean => {
+ export const hasCapability = (feature: CompatibilityFeature): boolean => {
   const version = config.buildInfo.version;
 
   switch (feature) {
@@ -23,18 +22,4 @@ enum CompatibilityFeature {
     default:
       return false;
   }
-};
-
-/**
- * Calls the override testDatasource function for backwards compatibility if needed.
- *
- * @param baseTestDatasource The original testDatasource function
-* @returns The result in the expected format for the Grafana version
- */
- export const healthDiagnosticsErrorsCompat = (baseTestDatasource: BaseTestDatasource) => {
-  if (hasCapability(CompatibilityFeature.HEALTH_DIAGNOSTICS_ERRORS)) {
-    return baseTestDatasource()
-  }
-
-  return testDatasource(baseTestDatasource);
 };
