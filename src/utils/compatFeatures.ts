fdec34a9f89161252ab32198c9e60d9c51ae9f3d
Vicky Lee <vicky.winkay.lee@gmail.com>
2021-05-06 13:26:37 +0100

    update jsdocs

--- src/utils/compatFeatures.ts
+++ src/utils/compatFeatures.ts
@@ -5,6 +5,7 @@ import { BaseTestDatasource, testDatasource, TestDatasourceReturn } from "./test
  * Calls the override testDatasource function for backwards compatibility if needed.
  *
  * @param baseTestDatasource The original testDatasource function
+ * @param toggle Accepts a feature toggle. Defaults to false so it is clear when we want this feature turned on.
  * @returns The result in the expected format for the Grafana version
  */
 export const healthDiagnosticsErrorsCompat = (baseTestDatasource: BaseTestDatasource, toggle?: boolean): Promise<TestDatasourceReturn> => {
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,2 +1,3 @@
 export * from "./compatibility"
+export * from "./compatFeatures"
 export * as compatFeatures from "./compatFeatures"
