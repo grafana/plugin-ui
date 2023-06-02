8bf6fcd0185eac0c62820cfcf10f02e290f32d28
Andriy Urbanas <andriy.urbanas@grafana.com>
2023-06-02 20:27:35 +0200

    feat: add changelog functions to avoid commit hash in release lines

---
+++ src/utils/changeset/functions.ts
@@ -0,0 +1,17 @@
+import {
+  GetReleaseLine,
+  GetDependencyReleaseLine,
+  ChangelogFunctions,
+} from "@changesets/types";
+
+const getReleaseLine: GetReleaseLine = async (changeset) =>
+  `- ${changeset.summary}`;
+
+const getDependencyReleaseLine: GetDependencyReleaseLine = async () => "";
+
+const defaultChangelogFunctions: ChangelogFunctions = {
+  getReleaseLine,
+  getDependencyReleaseLine,
+};
+
+export default defaultChangelogFunctions;
