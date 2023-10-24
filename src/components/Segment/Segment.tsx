19e8b087005ba81dac911f8d2ffc8abf0e9e4de6
Andriy <andriy.urbanas@grafana.com>
2023-10-24 18:21:36 +0200

    chore: upgrade dependencies (#82)

--- src/components/Segment/Segment.tsx
+++ src/components/Segment/Segment.tsx
@@ -1,10 +1,9 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, ComponentProps } from 'react';
 import { Segment as GrafanaSegment } from '@grafana/ui';
 import { SelectableValue } from '@grafana/data';
-import { SegmentSyncProps as GrafanaSegmentProps } from '@grafana/ui/components/Segment/Segment';
 import { useDebounce } from '../../hooks/useDebounce';
 
-export interface SegmentProps<T> extends Omit<GrafanaSegmentProps<T>, 'onChange'> {
+export interface SegmentProps<T> extends Omit<ComponentProps<typeof GrafanaSegment<T>>, 'onChange'> {
   value: T;
   onDebounce: (debouncedSegment?: T) => void;
   delay?: number;
