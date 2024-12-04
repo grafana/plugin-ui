396504824c1bd482b0f0d5e3371a5bd06b32f8ea
Darren Janeczek <darren.janeczek@grafana.com>
2024-12-04 16:26:02 -0500

    fix: common types for tls editor and utils

--- src/components/ConfigEditor/types.ts
+++ src/components/ConfigEditor/types.ts
@@ -1,6 +1,14 @@
 import { DataSourceSettings, DataSourceJsonData, PluginMeta } from '@grafana/data';
 
-export type Config<JSONData extends DataSourceJsonData = any, SecureJSONData = any> =
-  | DataSourceSettings<JSONData, SecureJSONData>
-  | PluginMeta<JSONData>;
+export type Config<JSONData extends DataSourceJsonData = any, SecureJSONData = any> = DataSourceSettings<
+  JSONData,
+  SecureJSONData
+>;
+
+export type AppConfig<JSONData extends DataSourceJsonData = any> = PluginMeta<JSONData>;
+
+export type CommonConfig = Config | AppConfig;
+
 export type OnChangeHandler<C extends Config = Config> = (config: C) => void;
+
+export type OnCommonChangeHandler<C extends CommonConfig = CommonConfig> = (config: C) => void;
