af97600986290604910f01c46bd94fc4bcc7025f
Ivana Huckova <ivana.huckova@gmail.com>
2025-01-28 11:34:54 +0100

    Remove AsyncButtonCascader, Cascader, InlineSwitch and CertificationKey components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,3 @@
-export { AsyncButtonCascader } from './AsyncButtonCascader/AsyncButtonCascader';
 export { QueryEditorRow } from './QueryEditorRow/QueryEditorRow';
 export { DebounceInput, DebounceInputProps } from './DebounceInput/DebounceInput';
 export { Segment, SegmentProps } from './Segment/Segment';
@@ -6,8 +5,6 @@ export { DatePicker, DatePickerProps } from './DatePicker/DatePicker';
 export { DatePickerWithInput, DatePickerWithInputProps, formatDate } from './DatePickerWithInput/DatePickerWithInput';
 export * from './DataSourcePicker/DataSourcePicker';
 export * from './DataLinks';
-export * from './Cascader/Cascader';
-export { InlineSwitch, CertificationKey } from '@grafana/ui';
 export * from './QueryEditor';
 export * from './ConfigEditor';
 export { CustomHeadersSettings } from './CustomHeadersSettings/CustomHeadersSettings';
